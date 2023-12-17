f1aacffa95bc46bbef9b490247a917cde0ca5230
Adrian Dușa <dusa.adrian@gmail.com>
2023-12-17 13:57:19 +0200

    white space modifications

--- R/extractDisjunctions.R
+++ R/extractDisjunctions.R
@@ -1,26 +1,25 @@
 `extractDisjunctions` <- function(x, snames) {
-    
+
     # purpose: allow complete disjunctions to be plotted
     # in version 1.5, something like "A + (B + C)" is treated as "A + B + C"
     if (missing(snames)) {
         snames <- ""
     }
-    
-    if (nchar(gsub("[A-Za-z]|\\+|\\*|\\(|\\)", "", x)) > 0) {
-        cat("\n")
-        stop(simpleError("Invalid expression.\n\n"))
+
+    if (nchar(gsub("[A-Za-z]|\\+|\\*|\\(|\\)| |~", "", x)) > 0) {
+        admisc::stopError("Invalid expression.")
     }
-    
+
     transformx <- function(x) {
         unname(apply(x, 1, function(x) {
             x[x < 0] <- "-"
             paste(x, collapse = "")
         }))
     }
-    
+
     y <- admisc::translate(x, snames = snames)
     snames <- colnames(y)
-    
+
     if (grepl("\\(|\\)", x)) {
         opened <- gsub("[A-Za-z]|\\+|\\*|\\(", "", x)
         closed <- gsub("[A-Za-z]|\\+|\\*|\\)", "", x)
@@ -28,12 +27,12 @@
             cat("\n")
             stop(simpleError("All opened / closed brackets should match.\n\n"))
         }
-        
+
         if (grepl("\\)\\*\\(|\\)\\(", x)) {
             cat("\n")
             stop(simpleError("Brackets are allowed only for disjunctions.\n\n"))
         }
-        
+
         sx <- unlist(strsplit(x, split = ""))
         wf <- which(sx == "(")[1]
         if (wf != 1) {
@@ -42,11 +41,11 @@
                 stop(simpleError("Brackets are allowed only for disjunctions.\n\n"))
             }
         }
-        
+
         x <- unlist(strsplit(x, split = ""))
         opened <- which(x == "(")
         closed <- which(x == ")")
-        
+
         if (opened[1] == 1) {
             if (closed[length(closed)] != length(x)) {
                 cat("\n")
@@ -58,14 +57,14 @@
                 closed <- closed[-length(closed)]
             }
         }
-        
+
         if (length(opened) > 0) {
-            
+
             if (opened[1] == 1) {
                 cat("\n")
                 stop(simpleError("Invalid expression.\n\n"))
             }
-            
+
             result <- list(transformx(admisc::translate(paste(x[seq(1, opened[1] - 1)], collapse = ""), snames = snames)))
             for (i in seq(length(opened))) {
                 result[[i + 1]] <- transformx(admisc::translate(paste(x[seq(opened[i] + 1, closed[i] - 1)], collapse = ""), snames = snames))
@@ -75,10 +74,10 @@
                     cat("\n")
                     stop(simpleError("Brackets are allowed only for disjunctions.\n\n"))
                 }
-                
+
                 result[[length(result) + 1]] <- transformx(admisc::translate(paste(x[seq(closed[length(closed)] + 2, length(x))], collapse = ""), snames = snames))
             }
-            
+
             return(result)
         }
         else {
@@ -88,5 +87,5 @@
     else {
         return(list(transformx(y)))
     }
-    
+
 }
